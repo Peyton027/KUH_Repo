644d25d13a6f37c778c9f1943fd29740cfed5f2e
Peyton L Chiu <plchiu@ucsd.edu>
2026-07-09 14:16:25 -0500

    Add PCA analysis script for RNA data

---
+++ scripts/omics/PCA.r
@@ -0,0 +1,49 @@
+library(factoextra)
+library(MASS)
+library(dplyr)
+library(ggplot2)
+
+# load data 
+raw_rna <- read.csv("RNA_ion_channels.csv")
+head(raw_rna)
+dim(raw_rna)
+
+
+#keep only genes with nonzero variance
+genes <-apply(raw_rna,1,var, na.rm=T)!=0
+rna <-rna[genes,]
+dim(rna)
+head(rna)
+
+
+#normalize
+
+smpl_size <-colSums(rna)
+rna_cpm <-t(t(rna)/smpl_size *1000000)
+
+#log2 transform 
+rna_log <- log2(rna + 1)
+
+#transpose matrix 
+pca <- prcomp(t(rna_log), scale. = TRUE)
+
+#add condition class 
+sample_names <- rownames(pca$x)
+
+condition <- ifelse(
+  grepl("^N", sample_names),
+  "Normal",
+  "HF"
+)
+
+#scree plot 
+fviz_eig(pca,addlabels = TRUE)
+
+#pca plot
+fviz_pca_ind(
+  pca,
+  geom.ind = "point",
+  habillage = condition,
+  addEllipses = TRUE,
+  repel = T
+)
