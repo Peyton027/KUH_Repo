ee895f71ff68fa9fc336f8bd0c287097b534e55a
Peyton L Chiu <plchiu@ucsd.edu>
2026-07-10 16:38:55 -0500

    Refactor PCA script for RNA analysis

--- scripts/omics/PCA.r
+++ scripts/omics/PCA.r
@@ -1,37 +1,50 @@
+{r}
+
 library(factoextra)
 library(MASS)
 library(dplyr)
 library(ggplot2)
 
-# load data 
-raw_rna <- read.csv("RNA_ion_channels.csv")
-rownames(raw_rna) <- raw_rna$gene_id
-raw_rna <- raw_rna[, -1]
+# load all genes
+all_rna <- read.csv("RNA seq raw file.csv")
 
-head(raw_rna)
-dim(raw_rna)
+# load ion-channel genes
+ion <- read.csv("RNA_ion_channels.csv")
 
+# save gene id as row names
+rownames(all_rna) <- all_rna$gene_id
+rownames(ion) <- ion$gene_id
 
-#keep only genes with nonzero variance
-genes <-apply(raw_rna,1,var, na.rm=T)!=0
-rna <-rna[genes,]
-dim(rna)
-head(rna)
+# get Normal and HF sample names from ion-channel file
+sample_names <- colnames(ion)[-1]
 
+# keep only the same Normal and HF sample columns
+all_rna <- all_rna[, sample_names]
+ion <- ion[, sample_names]
 
+# find library size using all genes
+smpl_size <- colSums(all_rna)
 
-#normalize
+# normalize ion-channel genes
+rna_cpm <- t(t(ion) / smpl_size * 1000000)
 
-smpl_size <-colSums(rna)
-rna_cpm <-t(t(rna)/smpl_size *1000000)
-
-#log2 transform 
+# log2 transform
 rna_log <- log2(rna_cpm + 1)
 
-#transpose matrix 
-pca <- prcomp(t(rna_log), scale. = TRUE)
+# get rid of 0 variance genes
+genes <- apply(rna_log, 1, var, na.rm = TRUE) != 0
+rna_log <- rna_log[genes, ]
 
-#add condition class 
+dim(rna_log)
+head(rna_log)
+
+# transpose matrix
+pca <- prcomp(
+  t(rna_log),
+  scale. = TRUE
+)
+
+# add condition class
 sample_names <- rownames(pca$x)
 
 condition <- ifelse(
@@ -40,14 +53,19 @@ condition <- ifelse(
   "HF"
 )
 
-#scree plot 
-fviz_eig(pca,addlabels = TRUE)
+# scree plot
+fviz_eig(
+  pca,
+  addlabels = TRUE
+)
 
-#pca plot
+# PCA plot
 fviz_pca_ind(
   pca,
   geom.ind = "point",
   habillage = condition,
   addEllipses = TRUE,
-  repel = T
+  repel = TRUE
 )
+
+
